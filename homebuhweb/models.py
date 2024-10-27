db6bc8735698012692dcfce4dd0c451ccefe85e8
Lara <kulikova2.22larisa@gmail.com>
2024-10-27 05:37:46 +0300

    удалены не нужные модели

--- homebuhweb/models.py
+++ homebuhweb/models.py
@@ -8,58 +8,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class MyUserManager(BaseUserManager):
-    def create_user(self, email, username, password=None):
-        if not email:
-            raise ValueError('Обязательное поле. Введите адрес электронной почты')
-        if not username:
-            raise ValueError('Обязательное поле. Введите имя пользователя')
-
-        user = self.model(
-            email=self.normalize_email(email),
-            username=username,
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, username, password=None):
-        user = self.create_user(
-            email,
-            username=username,
-            password=password,
-        )
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
-
-
-class MyUser(AbstractBaseUser):
-    email = models.EmailField(verbose_name='email address', max_length=255, unique=True)
-    username = models.CharField(max_length=30, unique=True)
-    is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
-
-    objects = MyUserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-
-    def __str__(self):
-        return self.email
-
-    def has_perm(self, perm, obj=None):
-        return True
-
-    def has_module_perms(self, app_label):
-        return True
-
-    @property
-    def is_staff(self):
-        return self.is_admin
-
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
